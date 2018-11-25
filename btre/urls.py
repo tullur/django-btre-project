c8d71d4c6c307d918e04c9af5266b6be42ea39d7
psidae <akson.rill@gmail.com>
2018-11-25 18:59:50 +0100

    add search option

--- btre/urls.py
+++ btre/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),

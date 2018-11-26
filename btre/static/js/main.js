f98a653160ae0e4a93fd3ddd188497f7e7c502ad
psidae <akson.rill@gmail.com>
2018-11-26 18:31:49 +0100

    add titles/login & logout

--- btre/static/js/main.js
+++ btre/static/js/main.js
@@ -2,5 +2,5 @@ const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
 setTimeout(function () {
-    $('message').fadeOut('slow');
-}, 5000);
+    $('#message').fadeOut('slow');
+}, 3000);

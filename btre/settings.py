b563480563a5e0ffd8095ab16f9ef7ba0d917ed2
psidae <akson.rill@gmail.com>
2018-11-27 17:26:06 +0100

    whitenoise changes

--- btre/settings.py
+++ btre/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
